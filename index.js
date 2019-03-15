bfc3478bde7f2101d75ad833f2766519933ed3fa
Matt Hillsdon <matt@hillsdon.net>
2019-03-15 21:39:57 +0000

    Use circleci-api.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-const fetch = require("node-fetch");
+const { GitType, CircleCI } = require("circleci-api");
 const YAML = require("YAML");
 
-const { exec } = require("child_process");
-const querystring = require("querystring");
+const { execFile } = require("child_process");
 const fs = require("fs");
 const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
@@ -23,40 +22,9 @@ const readToken = async () => {
   return token;
 };
 
-const createApiClient = ({ token }) => {
-  return async (path, query) => {
-    const url = `https://circleci.com/api/v1.1/${path}?${querystring.stringify({
-      ...(query || {}),
-      "circle-token": token
-    })}`;
-    const headers = { headers: "accept: application/json" };
-    const result = await fetch(url, { headers });
-    if (result.status !== 200) {
-      throw new Error(
-        `Unexpected CircleCI API response: ${result.status} ${
-          result.statusText
-        }`
-      );
-    }
-    return result.json();
-  };
-};
-
-const createCircleClient = apiClient => {
-  return {
-    recentBuilds: ({ username, project, branch }) => {
-      const vcsType = "github";
-      const path = `project/${vcsType}/${username}/${project}/tree/${encodeURIComponent(
-        branch
-      )}`;
-      return apiClient(path, { limit: 1 });
-    }
-  };
-};
-
-const execAsync = (command, callback) => {
+const execFileAsync = async (file, args) => {
   return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
+    execFile(file, args, (error, stdout, stderr) => {
       if (error) {
         reject(stderr);
       } else {
@@ -67,35 +35,43 @@ const execAsync = (command, callback) => {
 };
 
 const parseGitUrl = url => {
-  // We could teach it about other providers and https variants.
+  // We could teach this about other providers and https variants.
   const match = /^git@github.com:(.+)\/(.+).git$/.exec(url);
   if (!match) {
     throw new Error(`Unsupported Git remote URL ${url}`);
   }
   return {
-    username: match[1],
-    project: match[2]
+    type: GitType.GITHUB,
+    owner: match[1],
+    repo: match[2]
   };
+};
+
+const openUrl = async url => {
+  if (!url.startsWith("https://")) {
+    throw new Error(`Ignoring unexpected URL format: ${url}`);
+  }
+  await execFileAsync("open", [url]);
 };
 
 const main = async () => {
   const token = await readToken();
-  const gitOriginUrl = (await execAsync("git remote get-url origin")).trim();
-  const branch = (await execAsync("git rev-parse --abbrev-ref HEAD")).trim();
-  const urlInfo = parseGitUrl(gitOriginUrl);
-  const client = createCircleClient(createApiClient({ token }));
-  const recentBuilds = await client.recentBuilds({
-    ...urlInfo,
-    branch
-  });
+  const gitOriginUrl = (await execFileAsync("git", ["remote", "get-url", "origin"])).trim();
+  const branch = (await execFileAsync("git", ["rev-parse", "--abbrev-ref", "HEAD"])).trim();
+  const vcs = parseGitUrl(gitOriginUrl);
+  const options = {
+    token,
+    vcs,
+    options: {
+      branch,
+    }
+  }
+  const circleci = new CircleCI(options);
+  const recentBuilds = await circleci.recentBuilds({limit: 1});
   if (recentBuilds.length === 0) {
     throw new Error("No builds for this branch.");
   }
-  const buildUrl = recentBuilds[0].build_url;
-  if (!buildUrl.startsWith("https://")) {
-    throw new Error(`Ignoring unexpected URL format: ${buildUrl}`);
-  }
-  await execAsync("open " + buildUrl);
+  await openUrl(recentBuilds[0].build_url);
 };
 
 (async () => {
